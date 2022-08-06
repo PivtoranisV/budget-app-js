d851e039b72aa753295770178233543abd225d25
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-08-06 07:58:37 +0400

    Added function to show/hide tabs in Dashboard

--- budget.js
+++ budget.js
@@ -7,3 +7,45 @@ const chartEl = document.querySelector(".chart");
 const expenseBtn = document.querySelector(".tab1");
 const incomeBtn = document.querySelector(".tab2");
 const allBtn = document.querySelector(".tab3");
+
+const expenseEl = document.querySelector("#expense");
+const incomeEl = document.querySelector("#income");
+const allEl = document.querySelector("#all");
+
+//EVENT Listeners
+expenseBtn.addEventListener("click", function () {
+  show(expenseEl);
+  hide([incomeEl, allEl]);
+  active(expenseBtn);
+  inactive([incomeBtn, allBtn]);
+});
+incomeBtn.addEventListener("click", function () {
+  show(incomeEl);
+  hide([expenseEl, allEl]);
+  active(incomeBtn);
+  inactive([expenseBtn, allBtn]);
+});
+allBtn.addEventListener("click", function () {
+  show(allEl);
+  hide([incomeEl, expenseEl]);
+  active(allBtn);
+  inactive([incomeBtn, expenseBtn]);
+});
+
+//FUNCTIONS
+function active(element) {
+  element.classList.add("active");
+}
+function show(element) {
+  element.classList.remove("hide");
+}
+function hide(elementsArray) {
+  elementsArray.forEach((element) => {
+    element.classList.add("hide");
+  });
+}
+function inactive(elements) {
+  elements.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
