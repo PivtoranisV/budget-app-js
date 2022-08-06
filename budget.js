92b49230b8f987d619ec0674ef403eb36eac1a94
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-08-06 07:27:54 +0400

    Add select elements to javascript

---
+++ budget.js
@@ -0,0 +1,9 @@
+// SELECT elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const chartEl = document.querySelector(".chart");
+
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
